0027ce339bfdc5424072fc730c341fd866b92001
Keerthu781 <80473077+Keerthu781@users.noreply.github.com>
2021-05-27 19:25:01 +0530

    create product.cpp
    It helps to find the product of two numbers.

---
+++ product.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+    {
+	float n,n1,p;
+	cout<<"\n\t The product of two numbers";
+	cout<<"\n\t  _________________________";
+	cout<<"\n\tEnter the first number :";
+	cin>>n;
+	cout<<"\n\tEnter the second number:";
+	cin>>n1;
+	p=n*n1;
+	cout<<"\n\tThe product of two numbers:"<<p;
+	
+	return 0;
+   }
